7a1714507a7d75c6aee0bc445191a1d1a481e641
Cyrus-Muchiri <cyrusmuchirichomba@gmail.com>
2021-05-31 23:17:22 +0300

    Problem debunking

---
+++ technobrain.cs
@@ -0,0 +1,28 @@
+/* 
+@author Cyrus Muchiri
+@date 5/31/2021
+for Technobrain Interview
+ */
+Using system;
+
+public class Employees
+{
+     public Employees(Parameters)
+    {
+        /* Take CSV input */
+        /* Validate that an employee does not report to more than one manager */
+        /* There is only one manager */
+        /* There is no circular reference */
+        /* There is no manager who is not an employee */
+    }
+    
+    public long salaryBudgetManager(String name) {
+        /* Returns the salary budget from the specified manager */
+        /* Salary budget is sum of all employees plus manager */
+    }
+
+    public static void main(String[] args){
+        /* Create Object */
+        /* Call instance method */
+    }
+}
